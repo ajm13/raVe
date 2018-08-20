8eeeed99840bd98ad14ae0e4305f8cc89c246eba
Alex McNerney <ajm13@me.com>
2018-08-19 18:36:26 -0700

    Nav improvements

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
 <template>
   <div id="app">
     <div class="container">
-      <div class="nav">
+      <transition
+        appear
+        mode="out-in"
+        :leave-class="transitionClass('leave')"
+        :leave-active-class="transitionClass('leave-active')"
+        :leave-to-class="transitionClass('leave-to')"
+        :enter-class="transitionClass('enter')"
+        :enter-active-class="transitionClass('enter-active')"
+        :enter-to-class="transitionClass('enter-to')"
+      >
+        <router-view />
+        </transition>
+    </div>
+    <div class="nav">
+      <div class="nav-links">
         <router-link to="/">ra<span>V</span>e</router-link>
         <router-link to="setup">setup</router-link>
         <router-link to="about">about</router-link>
         <router-link to="history">history</router-link>
         <router-link to="donate">donate</router-link>
-      </div>
-      <div class="content">
-        <transition
-          appear
-          mode="out-in"
-          :leave-class="transitionClass('leave')"
-          :leave-active-class="transitionClass('leave-active')"
-          :leave-to-class="transitionClass('leave-to')"
-          :enter-class="transitionClass('enter')"
-          :enter-active-class="transitionClass('enter-active')"
-          :enter-to-class="transitionClass('enter-to')"
-        >
-          <router-view />
-          </transition>
       </div>
     </div>
   </div>
@@ -42,6 +42,7 @@ export default {
 
   methods: {
     updateTransition(to, from, next) {
+      window.scrollTo(0, 0)
       this.fromTransition = from.meta.transition || DEFAULT_TRANSITION
       this.toTransition = to.meta.transition || DEFAULT_TRANSITION
       if (next) next()
@@ -64,7 +65,7 @@ export default {
   justify-content: center;
 
   width: 100vw;
-  height: 100vh;
+  min-height: 100vh;
 
   font-family: 'Comfortaa', sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -72,38 +73,65 @@ export default {
 }
 
 .container {
-  display: flex;
-  flex-flow: column;
-
   width: 100%;
   max-width: 800px;
-  height: 100%;
-}
 
-.content {
-  flex: 1 1 100%;
   padding: 1em 2em;
+  padding-top: 5em;
   background: rgba(#000, 0.8);
 }
 
 .nav {
-  position: sticky;
+  position: fixed;
   top: 0;
+  left: 0;
 
-  flex: 0 0 auto;
-  display: flex;
+  width: 100%;
+  transform: translate3d(0, 0, 0);
 
-  padding: 1em;
-  background: #111;
+  &-links {
+    width: 100%;
+    max-width: 800px;
 
-  a {
-    color: inherit;
-    text-decoration: none;
-    padding: 0.5em;
-    margin: 0 1em;
+    z-index: 1000;
+    display: flex;
 
-    &.router-link-exact-active {
-      box-shadow: 0 2px 0 #ddd;
+    margin: 0 auto;
+    padding: 1em;
+
+    background: #111;
+
+    a {
+      position: relative;
+
+      padding: 0.5em;
+      margin: 0 1em;
+
+      color: inherit;
+      text-decoration: none;
+
+      &:after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+
+        width: 0%;
+        height: 2px;
+
+        background: #888;
+        transition: all 0.3s ease;
+      }
+
+      &:hover:after,
+      &.router-link-exact-active:after {
+        width: 100%;
+      }
+
+      &.router-link-exact-active:after {
+        background: #ddd;
+      }
     }
   }
 }
