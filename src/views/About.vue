64137963f89d8efce851d2fe990758825de3556c
Alex McNerney <ajm13@me.com>
2018-11-16 15:01:42 -0800

    About typo fix

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,9 +3,9 @@
     <h1>About</h1>
     <section>
       <h2>What is
-        <rave/>?</h2>
+        <rave />?</h2>
       <p>
-        <rave/> stands for
+        <rave /> stands for
         <strong>realtime audio visualizer experience</strong>.
       </p>
       <p>
@@ -15,25 +15,25 @@
         different aspects of it will change when a beat is detected in the song.
       </p>
       <p>
-        <rave/> isn't an animation that reacts to the beat, but rather a true visualization
+        <rave /> isn't an animation that reacts to the beat, but rather a true visualization
         of the audio data itself. The frequencies and waveforms of the incoming audio
         are used to generate
-        <rave/>'s rings, and the previous frames expand out from the center to visually
+        <rave />'s rings, and the previous frames expand out from the center to visually
         display the history of the audio.
       </p>
       <p>
         Because
-        <rave/> uses all of the audio data available to it for visualization, every song
+        <rave /> uses all of the audio data available to it for visualization, every song
         is visually unique. The same goes for the vocals; singers' voices are visually
         distinct as they contain different harmonics and waveform types. Unique sounds
         in songs look amazing, and in my opinion, the best songs to watch on
-        <rave/> are chill songs with not too much going on.
+        <rave /> are chill songs with not too much going on.
       </p>
     </section>
     <section>
       <h2>Technical Details</h2>
       <p>
-        <rave/> uses the
+        <rave /> uses the
         <strong>Web Audio API</strong> and
         <strong>Canvas API</strong> to collect and draw the audio data in realtime.
       </p>
@@ -43,7 +43,7 @@
         audio through, including
         <strong>AnalyserNode</strong> and
         <strong>BiquadFilterNode</strong>. These two nodes are the key pieces to making
-        <rave/> work.
+        <rave /> work.
       </p>
       <p>
         <strong>AnalyserNode</strong> is a wonderful little piece of
@@ -57,27 +57,27 @@
         <strong>BiquadFilterNode</strong> is a node that you can connect audio through to
         apply different biquad filters, such as highpass or lowpass. These filters
         allow
-        <rave/> to filter out different frequencies to make the visuals more aesthetic.
+        <rave /> to filter out different frequencies to make the visuals more aesthetic.
         For example, the entire audio source is run through a lowpass filter of 5&nbsp;KHz,
         so that snares and "sss" noises don't brighten
-        <rave/> too much.
+        <rave /> too much.
       </p>
     </section>
     <section>
       <h2>Anatomy of
-        <rave/>
+        <rave />
       </h2>
       <p class="img">
         <img src="@/assets/anatomy.jpg" alt="anatomy of raVe">
       </p>
       <p>
-        <rave/> generates 2 rings from the audio data, and both rings slowly morph between
+        <rave /> generates 2 rings from the audio data, and both rings slowly morph between
         a circle and hexagon.
       </p>
       <p>The inner ring is the audio waveform applied to the ring's shape, and mirrored
         along the y-axis.
       </p>
-      <p>The outer ring is made up of both frequency and time domain data, and is symmetry
+      <p>The outer ring is made up of both frequency and time domain data, and is symmetric
         over both the x and y axes. The frequency data goes from 0&nbsp;Hz at the
         horizon to about 7&nbsp;KHz at the top and bottom. The time domain data used
         for the outer ring is filtered to only show bass frequencies.
